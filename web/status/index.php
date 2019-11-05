10d85a446f2dc1a55185fe05cc58b1aa943f8e2c
roseckyj <rosecky.jonas@centrum.cz>
2019-11-05 18:12:27 +0100

    ()

--- web/status/index.php
+++ web/status/index.php
@@ -3,7 +3,7 @@ header('Content-type: application/json');
 
 $html = file_get_contents('https://teamtrees.org/');
 preg_match('/<div id="totalTrees" class="counter" data-count="([0-9]+)">/gm', $html, $re);
-var_dump $re;
+var_dump($re);
 
 $array = [
   "trees" => $re[1]
